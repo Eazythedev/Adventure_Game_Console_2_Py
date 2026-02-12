122997a38daf2aae2e485ef8fdd741c6ad0f0810
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2026-02-12 05:44:51 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Adventure Game Console 2 - The Adventures of Grassblade Land
+# Created by Ezaiah Akyeem / Eazythedev / Erujine
+# Created on February 11, 2026
+
+print("...")
+print(".....")
+print("*Water swooshing*")
+print("*River water flowing smoothly and subtly*")
