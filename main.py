7cba77b084692bd5be0afd2c312388fea8e7c7dd
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-02-13 17:41:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,24 @@
 # Created on February 11, 2026
 
 print("...")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
 print(".....")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
 print("*Water swooshing*")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
 print("*River water flowing smoothly and subtly*")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
 print("*Sun shining*")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
+print("You arise from the shallow river, in a place that seems immediately familiar.")
+input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+
+print("You stand up in the river, water gently flowing by at your feet.")
+
+answer = input(" a"
+               "")
