1f54450f908896ab531d7f0e00dbc81f636ee24d
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-02-13 20:17:39 -0800

    Update main.py
    Changes includes:
    - Created a border for formatting and differentiaitng the game world from the user prompts.
    - Started creating logic for input and player choice.

--- main.py
+++ main.py
@@ -2,25 +2,81 @@
 # Created by Ezaiah Akyeem / Eazythedev / Erujine
 # Created on February 11, 2026
 
+print("_____________________________________________________________________________________________________")
 print("...")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print(".....")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print("*Water swooshing*")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print("*River water flowing smoothly and subtly*")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print("*Sun shining*")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print("You arise from the shallow river, in a place that seems immediately familiar.")
-input("\n\n\n" + "(Press the Enter key to continue)" + "\n")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
+print("_____________________________________________________________________________________________________")
 print("You stand up in the river, water gently flowing by at your feet.")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
 
-answer = input(" a"
-               "")
+print("_____________________________________________________________________________________________________")
+print("Looking at the side of the river, you see a bag under a tree and some extra clothes.")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
+
+print("_____________________________________________________________________________________________________")
+print("You peek inside of the bag, and it is empty. You put on the extra clothes and walk up the small hill" + "\n" +
+      " from the river to get to a field.")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
+
+print("_____________________________________________________________________________________________________")
+print("Leaned against a rock, you find a wooden sword, pick it up, inspect it, and take it with you.")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n\n")
+
+print("_____________________________________________________________________________________________________")
+print("From the field you can see a cave in the distance on the left and some ruins on the right.")
+print("_____________________________________________________________________________________________________")
+input("\n\n\n" + "(Press the 'Enter' key to continue)" + "\n")
+
+print("Will you 1. Go to the cave or 2. Go to the ruins?")
+print("Select by pressing either the '1' or '2' keys.")
+
+# choice1 = input()
+
+
+while True:
+    choice1 = input()
+
+    if choice1 == 1:
+        print("You get to the entrance of the cave and find a torch, but you need a way to light it before you can enter the cave." + "\n" +
+            "You then return to the field to look for materials. You decide to search the ruins.")
+        break
+
+    elif choice1 == 2:
+        print("You get to the ruins and encounter mummies which seem to be hostile. You attack and defeat them but get a few bumps and bruises in the process.")
+        break
+
+    else:
+        print("Invalid input. Please choose by pressing either the '1' or '2' keys.")
+
+print("Next thing")
